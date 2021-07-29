1760dcf40af9dcd570a2abcb55d1571e83ba1644
JosephASandoval <josephasandoval17@gmail.com>
2021-07-29 11:35:20 -0700

    added frontend

---
+++ whiteboard.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const header = document.getElementById("header");
+  console.log(header);
+  header.innerHTML = "This works too!";
+  alert("wowwww");
+});
